c1a01cf90cdcd8d5397f474206e2288bf46e8ad1
radiathal <laura.a.s.millar@gmail.com>
2026-07-14 18:22:28 +0300

    displays in terminal what has been processed and when.

--- just_dance.py
+++ just_dance.py
@@ -1,26 +1,14 @@
 import tensorflow as tf
-import tensorflow_hub as hub
-from tensorflow_docs.vis import embed   #REMOVE later (for gifs)
 import numpy as np
 import cv2
 
-#my adds
-import urllib.request
 import time
 import os
 
 # Import matplotlib libraries   
 from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-import matplotlib.patches as patches
 
-# Some modules to display an animation using imageio.  REMOVE later
-import imageio
-from IPython.display import HTML, display
-
-#
 from threading import Thread
 from threading import Event
 from queue import Queue
@@ -29,10 +17,11 @@ import gesture_detect
 
 target_fps = 4
 stop_event = Event()
+birth_t=0
 
 
 # for video detect, define functions for each thread
-def video_controller(image_q, file_name):
+def video_controller(image_q, file_name, birth_t):
   #open video and get framerate
   cap = cv2.VideoCapture(file_name)
   if not cap.isOpened():
@@ -43,27 +32,33 @@ def video_controller(image_q, file_name):
 
   frame_interval = video_fps / target_fps
 
+  frame = None
+
   next_frame_to_save = 0.0
   frame_idx = 0
-
+  i=0
   while not stop_event.is_set():
     try:
-      ret, frame = cap.read()
-      if not ret:
-        break
       if frame_idx >= next_frame_to_save:
+        #print(f"[{time.time()-birth_t:.4f}]: {i} taking image...")
+        ret, frame = cap.read()
+        if not ret:
+          break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image_q.put(frame)
         next_frame_to_save += frame_interval
+        #print(f"[{time.time()-birth_t:.4f}]: {i} taken image")
+        i += 1
       frame_idx += 1
     except KeyboardInterrupt:
       break
 
-def pose_detection_controller(image_q, display_q):
+def pose_detection_controller(image_q, display_q, birth_t):
+  i = 0
   while not stop_event.is_set():
     try:
       image = image_q.get()
-      #print(f"[{time.time()}]: image recieved")
+      print(f"[{time.time()-birth_t:.4f}]: {i} detecting image...")
       
       # Run model inference.
       keypoints_with_scores = gesture_detect.movenet(image)
@@ -73,6 +68,8 @@ def pose_detection_controller(image_q, display_q):
       output_overlay = gesture_detect.draw_prediction_on_image(np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
 
       display_q.put(output_overlay)
+      print(f"[{time.time()-birth_t:.4f}]: {i} detected image")
+      i+=1
     except KeyboardInterrupt:
       break
 
@@ -94,6 +91,8 @@ def pose_detection_controller(image_q, display_q):
 
 #starts other threads and handles display
 def main_func(video_file_name):
+  birth_t = time.time()
+  print((f"[{time.time()-birth_t:.4f}]: START"))
   #placeholder image 
   image_path = 'images/empty.jpg'
   image = tf.io.read_file(image_path)
@@ -108,27 +107,30 @@ def main_func(video_file_name):
   image_q = Queue()
   
   #init threads for different processes
-  video_thread   = Thread(target=video_controller, args=(image_q, video_file_name,))
-  pose_thread    = Thread(target=pose_detection_controller, args=(image_q, display_q,))
+  video_thread   = Thread(target=video_controller, args=(image_q, video_file_name, birth_t,))
+  pose_thread    = Thread(target=pose_detection_controller, args=(image_q, display_q, birth_t,))
   #display_thread = Thread(target=display_controller, args=(display_q, img_display, fig,))
   
   #start threads
   video_thread.start()
   pose_thread.start()
   #display_thread.start()
-
+  i=0
   while True:
     try:
       #run when you get processed image
       output_overlay = display_q.get()
-      #print(f"[{time.time()}]: start display")
+      print(f"[{time.time()-birth_t:.4f}]: {i} displaying image...")
 
       img_display.set_data(output_overlay)   # update instead of creating new imshow
       fig.canvas.draw()
       fig.canvas.flush_events()
       plt.pause(0.001)
+      print(f"[{time.time()-birth_t:.4f}]: {i} displayed image")
+      i+=1
     except KeyboardInterrupt:
       stop_event.set()
+      print((f"[{time.time()-birth_t:.4f}]: STOP"))
       break
 
 
