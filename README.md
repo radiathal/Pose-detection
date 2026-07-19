22dc4cd2d0aa93e09493f6722efa8cdcc5074dbe
radiathal <laura.a.s.millar@gmail.com>
2026-07-19 18:19:05 +0300

    removed setup.py, because this ai does not need it.

--- README.md
+++
@@ -1,1 +0,0 @@
-Note: run setup.py to download needed ai-model locally (only once)
--- setup.py
+++
@@ -1,5 +0,0 @@
-import tensorflow_hub as hub
-import tensorflow as tf
-
-module = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
-tf.saved_model.save(module, "./movenet_lightning_local", signatures=module.signatures)
